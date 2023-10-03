3b9a576f3e4238222ad63dd9276385540852c63e
Andres Marquez <andresmr955@gmail.com>
2023-10-03 15:01:20 -0400

    Reto de presupuesto

---
+++ 05_zgastosmensual.py
@@ -0,0 +1,6 @@
+presupuesto_Mes_Uno = input("Cual es el gasto mensual de tu primer mes? ")
+presupuesto_Mes_Dos = input("Cual es el gasto mensual de tu segundo mes? ")
+presupuesto_Mes_Tres = input("Cual es el gasto mensual de tu tercer mes? ")
+
+media =( int(presupuesto_Mes_Uno) + int(presupuesto_Mes_Dos) + int(presupuesto_Mes_Tres) )/ 3
+print("La media de tu presupuesto mensual es de ", media)
