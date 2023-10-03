b77a5e359aecc966914811299f66b4bcb70b0264
Andres Marquez <andresmr955@gmail.com>
2023-10-03 16:46:39 -0400

    Transformaciones

---
+++ 07_transformation.py
@@ -0,0 +1,24 @@
+name = "Nicolas"
+print(type(name))
+
+name = 12
+print(type(name))
+
+name = True
+print(type(name))
+
+print("Nicolas" + "Molina")
+print(10 + 12)
+print ("Nicolas" + "54")
+
+age = 12
+print("Mi edad es " +  str(age))
+frase = "Mi edad es "
+
+print(f"Mi edad es {age}")
+
+age = input("Ecri votre age => ")
+print(type(age))
+age = int(age)
+age += 10
+print(f'Tu edad en 10 anos sera {age}')
