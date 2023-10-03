03d7410e69067e1a64ccc89a8f328460f0494e9d
Andres Marquez <andresmr955@gmail.com>
2023-10-03 14:16:23 -0400

    Estas son las variables

---
+++ 03_types.py
@@ -0,0 +1,21 @@
+# Strings
+my_name = "Andres";
+my_second_name = 'David';
+print("Mi nombre es ", my_name, my_second_name)
+print(type(my_name))
+
+
+#int 
+my_age = 24;
+
+
+# Boolean 
+is_single = True
+print(is_single)
+print(type(is_single))
+
+#inputs
+
+my_age = input("Cual es tu edad: ")
+print("my age ", my_age)
+print(type(my_age))
