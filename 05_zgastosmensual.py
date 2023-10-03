525ae0c1f61c902e126e84fff0fa7bb5011f5d18
Andres Marquez <andresmr955@gmail.com>
2023-10-03 16:25:22 -0400

    Booleans

--- 05_zgastosmensual.py
+++ 05_zgastosmensual.py
@@ -3,4 +3,4 @@ presupuesto_Mes_Dos = input("Cual es el gasto mensual de tu segundo mes? ")
 presupuesto_Mes_Tres = input("Cual es el gasto mensual de tu tercer mes? ")
 
 media =( int(presupuesto_Mes_Uno) + int(presupuesto_Mes_Dos) + int(presupuesto_Mes_Tres) )/ 3
-print("La media de tu presupuesto mensual es de ", media)
+print("La media de tu presupuesto mensual es de ", media)  
