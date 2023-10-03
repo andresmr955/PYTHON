9525546f72fcb91e2d34808b9f8485aa531d77b1
Andres Marquez <andresmr955@gmail.com>
2023-10-03 12:38:48 -0400

    Comenzando python

---
+++ 01_print.py
@@ -0,0 +1,14 @@
+print("Hola, esto es python") 
+print("Hola, Soy Andres y tengo 24 anos") 
+
+print(12 + 5);
+print(10 - 5);
+print(10 * 5);
+print(10 / 5);
+
+#Comentarios
+#Esto es un comentario     
+
+"""
+Comentario varias lineas
+"""    
